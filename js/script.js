7274bbae7de017c415c1483ab0fa7efe3f5d9e2d
Wanderson Santos <wandersonsantospereira21@gmail.com>
2023-05-07 19:54:10 -0300

    config of contact page to allow send an email

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
 'use strict';
 
+//sidebar variables
 const toggleButton = document.querySelector('#btn');
 const sidebar = document.querySelector('#sidebar');
-
-const url = window.location.href;
-const navLinks = document.querySelectorAll('a');
 
 //Profile dropdown toggle buttom
 toggleButton.addEventListener('click', () => {
     sidebar.classList.toggle('active');
 });
 
-//Navigation links mark
-for (var i =0; i < navLinks.length; i++) {
-    let link = navLinks[i];
 
-    if (link.href === url) {
-        link.classList.add('active');
-    }
-}
+//navbar links variables
+
+
+
+
+//form variables
+const inputs = document.querySelectorAll('[data-form-input]');
+const formBtn = document.querySelector('.send_message_btn');
+const form = document.querySelector('.form');
+
+//add event to all forms input
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("input", function () {
+  
+      // check form validation
+      if (form.checkValidity()) {
+        formBtn.removeAttribute("disabled");
+      } else {
+        formBtn.setAttribute("disabled", "");
+      }
+  
+    });
+  }
